9e99ce1cb33a40094ab0d76059369f439af75194
dotnet-code <@dotnet-code>
2023-04-19 10:41:05 +0530

    commit from code202304194105

---
+++ test_a_2/Controllers/GoogleDriveController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Drive.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using test_a_2;
+
+namespace test_a_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoogleDriveController : ControllerBase
+    {
+        //Declare credentials to access Google Drive
+        private string[] Scopes = { DriveService.Scope.Drive };
+        private string ApplicationName = "Google Drive Configuration";
+
+        //Constructor to initialize Google Drive service
+        public GoogleDriveController()
+        {
+            //Creating credentials using json file
+            var credentials = GoogleCredential.FromFile("credentials.json");
+
+            //Creating Drive API service
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        // GET: api/GoogleDrive
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Google Drive Configuration Service is running" };
+        }
+    }
+}
---
+++ test_a_2/Controllers/SignupController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using test_a_2.Models;
+
+namespace test_a_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignupController : ControllerBase
+    {
+        // POST: api/Signup
+        [HttpPost]
+        public ActionResult Post([FromBody] User user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (user != null)
+                {
+                    UserDataAccessLayer objuser = new UserDataAccessLayer();
+                    objuser.AddUser(user);
+                    return Ok("User Added successfully");
+                }
+                else
+                {
+                    return BadRequest("User object is null");
+                }
+            }
+            catch
+            {
+                return BadRequest("Error while creating user");
+            }
+        }
+    }
+}
